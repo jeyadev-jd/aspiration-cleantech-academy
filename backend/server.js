f8e1ed01d3c133fb3da974ac41460766048234c1
jeyadev <jeyadev2006@gmail.com>
2026-06-26 15:23:51 +0530

    Add aspcvacademy.com to backend CORS allowlist

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,9 @@ app.use(helmet());
 const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:5173',
   'http://localhost:5174',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://aspcvacademy.com',
+  'https://www.aspcvacademy.com'
 ];
 
 app.use(cors({
